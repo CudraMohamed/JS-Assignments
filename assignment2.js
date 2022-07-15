7ef786f057356963028ecfea9e46ff619c734eaa
CudraMohamed <ncudramohamed@gmail.com>
2022-07-15 08:00:52 +0300

    changed return type

--- assignment2.js
+++ assignment2.js
@@ -27,11 +27,10 @@ class KioskCalc{
         this.quantity=quantity;
         this.fruitPriceList={"Orange":30, "Strawberry":60,"Raspberry":50};
         this.getCost=function(){
-            return`${quantity} ${fruit} for KES ${quantity*this.price}`
+            return`${quantity} ${fruit} for KES ${quantity*this.fruitPriceList.Orange}`
         }
     }
 }
-KioskCalc.prototype.price=50;
 
-var kioskOne=new KioskCalc("Raspberry",2);
+var kioskOne=new KioskCalc("Orange",2);
 console.log(kioskOne.getCost());
