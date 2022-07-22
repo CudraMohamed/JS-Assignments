ab769a7f7d6db1a86d301148e9e07e70b84fcddc
CudraMohamed <ncudramohamed@gmail.com>
2022-07-22 09:05:38 +0300

    added consoles

--- assignment3.js
+++ assignment3.js
@@ -3,12 +3,13 @@ class Mkulima{
     this.farms = [];
     this.groceryVendors = [];
     this.products = [];
+    
     this.addFarm = (farmId, farmName, farmer, phone, address) => {
     this.farms.push({farmId,farmName,farmer,phone,address})
     }
     this.removeFarm = (farmId) => {
-    let specific = this.farms.find(item => item.farmId ===farmId);
-    let farmIndex = this.farms.indexOf(specific);
+    let specificObj = this.farms.find(item => item.farmId ===farmId);
+    let farmIndex = this.farms.indexOf(specificObj);
     this.farms.splice(farmIndex, 1);
     }
     this.updateFarm = function(farmId,newFarmId,newFarmName,newFarmersName,newPhone,newAddress){
@@ -23,38 +24,51 @@ class Mkulima{
     console.log(this.farms.find(object=>object.farmId===Id))
     }
     this.addProduct = (productId, productName, price) => {
-    this.product.push({productId,productName,price})
+    this.products.push({productId,productName,price})
     
     }
     this.removeProduct = (productId) => {
-    let specific = this.product.find(item => item.productId ===productId);
-    let productIndex = this.product.indexOf(specific);
-    this.product.splice(productIndex, 1);
+    let specific = this.products.find(item => item.productId ===productId);
+    let productIndex = this.products.indexOf(specific);
+    this.product.splice(productIndex, 2);
     }
     this.updateProduct = function(productId,newProductId,newProductName,newPrice){
-    let specificProduct = this.product.find(item => item.productId ===productId)
+    let specificProduct = this.products.find(item => item.productId ===productId)
     specificProduct.productId =newProductId;
     specificProduct.productName = newProductName;
     specificProduct.price = newPrice;
     }
     this.getProduct = (Id)=>{
-    console.log(this.product.find(object=>object.productId===Id))
+    this.products.find(object=>object.productId===Id)
     }
     this.printProducts = ()=>{
-    console.log(this.product)
+    console.log(this.products)
     }
     this.calculateOrderCost = (productId,quantity)=>{
-    let specificProduct = this.product.find(object=>object.productId===productId);
+    let specificProduct = this.products.find(object=>object.productId===productId);
     console.log(`${quantity} ${specificProduct.productName} for Kshs ${quantity*specificProduct.price}`);
     }
     }
     }
     
-    let farm1 = new Mkulima();
-    farm1.addFarm("5A 0.52ha","Cudra","Mohamed","+254 767 783 890", "089-987");
-    farm1.addFarm("9C 0.93ha","Rudra","Morehermade","+254 767 890 890", "098-345");
-    farm1.addFarm("8B 0.25ha","Gudra","Moreher","+254 890 765 789", "234-567");
-    console.log(farm1.farms);
+    let myFarm= new Mkulima();
+    myFarm.addFarm("5A 0.52ha","Cudra","Mohamed","+254 767 783 890", "089-987");
+    myFarm.addFarm("9C 0.93ha","Rudra","Morehermade","+254 767 890 890", "098-345");
+    myFarm.addFarm("8B 0.25ha","Gudra","Moreher","+254 890 765 789", "234-567");
+    console.log(myFarm.farms);
     
-    farm1.removeFarm("8B 0.25ha");
-    console.log(farm1)
+    myFarm.removeFarm("8B 0.25ha");
+    console.log(myFarm.farms);
+
+    myFarm.addProduct("12a","Bag of Maize",20000)
+    console.log(myFarm)
+
+    myFarm.updateFarm("5A 0.52ha","5A 0.45ha","Lima Farm","Qudra","+254 777 777 777","098-097");
+    console.log(myFarm.farms)
+
+    myFarm.getProduct("12a");
+    console.log(myFarm.farms);
+
+    myFarm.printProducts();
+
+    myFarm.calculateOrderCost("12a",1);
